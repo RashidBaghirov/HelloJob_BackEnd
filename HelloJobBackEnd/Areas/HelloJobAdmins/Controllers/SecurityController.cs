2fbaa46917098016430863b01e010ff7e335d643
Rashid Baghirov <rashidtb@code.edu.az>
2023-06-11 18:35:54 +0400

    Done

---
+++ HelloJobBackEnd/Areas/HelloJobAdmins/Controllers/SecurityController.cs
@@ -0,0 +1,86 @@
+﻿using HelloJobBackEnd.DAL;
+using HelloJobBackEnd.Entities;
+using HelloJobBackEnd.ViewModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace HelloJobBackEnd.Areas.HelloJobAdmins.Controllers
+{
+    [Authorize(Roles = "superadmin, admin, moderator")]
+    [Area("HelloJobAdmins")]
+    public class SecurityController : Controller
+    {
+        private readonly UserManager<User> _usermanager;
+        private readonly SignInManager<User> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly HelloJobDbContext _context;
+
+        public SecurityController(UserManager<User> usermanager, SignInManager<User> signInManager, RoleManager<IdentityRole> roleManager, HelloJobDbContext context)
+        {
+            _usermanager = usermanager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+            _context = context;
+        }
+        public async Task<IActionResult> Security()
+        {
+            User user = await _usermanager.FindByNameAsync(User.Identity.Name);
+            if (user is null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ProfileVM profileVM = new()
+            {
+                Email = user.Email,
+                UserName = user.UserName,
+                FullName = user.FullName
+            };
+            return View(profileVM);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Security(ProfileVM profileVM)
+        {
+            if (!ModelState.IsValid)
+            {
+                foreach (string message in ModelState.Values.SelectMany(e => e.Errors).Select(e => e.ErrorMessage))
+                {
+                    ModelState.AddModelError("", message);
+                }
+            }
+            TempData["Security"] = false;
+            User user = await _usermanager.FindByNameAsync(User.Identity.Name);
+            if (!string.IsNullOrWhiteSpace(profileVM.ConfirmNewPassword) && !string.IsNullOrWhiteSpace(profileVM.NewPassword))
+            {
+                var passwordChangeResult = await _usermanager.ChangePasswordAsync(user, profileVM.CurrentPassword, profileVM.NewPassword);
+
+                if (!passwordChangeResult.Succeeded)
+                {
+                    foreach (var item in passwordChangeResult.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                    return View();
+                }
+            }
+
+            user.UserName = profileVM.UserName;
+            var result = await _usermanager.UpdateAsync(user);
+
+            if (!result.Succeeded)
+            {
+                foreach (var item in result.Errors)
+                {
+                    ModelState.AddModelError("", item.Description);
+                }
+
+                return View();
+            }
+            await _signInManager.SignOutAsync();
+            TempData["Security"] = true;
+            return RedirectToAction("Login", "adminlogin");
+        }
+    }
+}
